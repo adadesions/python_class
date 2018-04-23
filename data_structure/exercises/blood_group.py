adce5224d4bb013bc1ec9a64e50e1be154cfaa76
adadesions <adadesions@gmail.com>
2018-04-23 16:49:22 +0700

    complete

---
+++ data_structure/exercises/blood_group.py
@@ -0,0 +1,17 @@
+"""
+    Create List for people in blood groups
+    Groups -> A, B, O, AB
+    in each group including people name
+    
+    for dataset,
+        A -> 'arise', 'alice', 'auto'
+        B -> 'Boron', 'Bertnard', 'Better'
+        O -> 'Oreo', 'Otaro', 'Otus'
+
+    Questions
+        1. Print first person name for each groups
+        2. Add 'Brown' to group B
+        3. Add new AB group to list
+        4. Copy all name in Group A, B to Group AB
+        5. Delete all name in Group A
+"""
